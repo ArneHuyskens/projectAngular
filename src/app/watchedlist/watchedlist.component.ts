1f0fb7f32f17b8c172e2a00b04a6e6702c0f5a23
Arne Huyskens <r0842910@student.thomasmore.be>
2022-11-25 16:20:21 +0100

    Kleine ui cleanup

--- src/app/watchedlist/watchedlist.component.ts
+++ src/app/watchedlist/watchedlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-watchedlist',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatchedlistComponent implements OnInit {
 
-  constructor() { }
+  movies: any;
+  movie: any;
+
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:3000/dbMovies').subscribe(dat => {
+      this.movies = dat;
+      console.warn(dat)
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  removeMovieFromWatched(movie: any) {
+    this.http.get('http://localhost:3000/dbMovies/' + movie.id).subscribe((dat) => {
+      this.movie = dat
+    });
+    this.http.put('http://localhost:3000/dbMovies/' + movie.id, {
+      Title: movie.Title, 
+      Poster: movie.Poster, 
+      Year: movie.Year, 
+      inWatched: false, 
+      inWatchList: true,
+      Rating: null,
+      Reaction: null
+    }).subscribe()
+    window.location.reload();
+  }
 }
